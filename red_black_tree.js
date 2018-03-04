269029082239658c5515143b373730876d41344a
blowder <blowder@mail.ru>
2018-03-05 00:10:08 +0200

    simple R/B rotation methods and recolor methods were added

--- red_black_tree.js
+++ red_black_tree.js
@@ -9,6 +9,17 @@ function Node(parent, value, color) {
     this.right = null;
 }
 
+
+Node.prototype.displayInOrder = function () {
+    if (this.left != null) {
+        this.left.displayInOrder()
+    }
+    console.log(this.value + ":" + (this.color ? "Red" : "Black"))
+    if (this.right != null) {
+        this.right.displayInOrder()
+    }
+}
+
 function insert(root, value) {
     if (root == null) {
         return new Node(null, value, BLACK);
@@ -18,7 +29,55 @@ function insert(root, value) {
     } else if (root.value < value) {
         root.right = _updateChild(root, root.right, value);
     }
+    //RB fixup tree
+    if (!isRed(root.left) && isRed(root.right))
+        root = rotateLeft(root);
+    if (isRed(root.left) && isRed(root.left.left))
+        root = rotateRight(root);
+    if (isRed(root.left) && isRed(root.right))
+        root = recolorTree(root);
     return root;
+}
+
+function isRed(node) {
+    return node != null && node.color == RED;
+}
+
+function rotateLeft(node) {
+    var result = node.right;
+    node.right = result.left;
+    result.left = node;
+    //inherit parent color
+    result.color = node.color;
+    node.color = RED;
+    //swap parents
+    result.parent = node.parent;
+    node.parent = result;
+    return result;
+}
+function rotateRight(node) {
+    var result = node.left;
+    node.left = result.right;
+    result.right = node;
+    //inherit parent color
+    result.color = node.color;
+    node.color = RED;
+    //swap parents
+    result.parent = node.parent;
+    node.parent = result;
+    return result;
+}
+
+function recolorTree(node) {
+    if (node == null) {
+        return null;
+    }
+    if (node.parent != null) {
+        node.color = RED;
+    }
+    node.left.color = BLACK;
+    node.right.color = BLACK;
+    return node;
 }
 
 function _updateChild(parent, child, value) {
@@ -34,10 +93,29 @@ function getBlackHeight(node) {
         return (node.color == RED ? 0 : 1) + Math.max(getBlackHeight(node.left), getBlackHeight(node.right))
     }
 }
+function printLevels(level) {
+    var nextLevel = [];
+    var levelPrint = ""
+    while (level.length != 0) {
+        var node = level.shift();
+        if (node.left != null) {
+            nextLevel.push(node.left);
+        }
+        if (node.right != null) {
+            nextLevel.push(node.right);
+        }
+        levelPrint += "[" + node.value + ":" + (node.color ? "Red" : "Black") + "]";
+    }
+    console.log(levelPrint);
+    if(nextLevel.length !=0)
+        printLevels(nextLevel);
+}
 
 var root = insert(null, 5);
-root = insert(root, 2);
-root = insert(root, 1);
-root = insert(root, 0);
-
-console.log(getBlackHeight(root));
+for (var i = 0; i < 10; i++) {
+    var nodeValue = Math.floor(Math.random() * 100)
+    root = insert(root, nodeValue);
+}
+//root = insert(root, 0.5);
+printLevels([root]);
+root.displayInOrder();
