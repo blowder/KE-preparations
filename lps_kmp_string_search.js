5e9ba1df35d73c96260d6509b3071a2e0101186d
blowder <blowder@mail.ru>
2018-03-15 01:23:44 +0200

    correct lps function was implemented

--- lps_kmp_string_search.js
+++ lps_kmp_string_search.js
@@ -1,25 +1,48 @@
 //longest suffix-prefix
 
 function calcLpsTable(pattern) {
-    var result = [];
-    for (var suffixSize = 1; suffixSize < pattern.length + 1; suffixSize++) {
-        var leftBorder = 0;
-        var rightBorder = suffixSize - 1
-        var cursor = 0;
-        var lps = 0;
-        while (cursor < suffixSize && rightBorder != 0) {
-            if (pattern[leftBorder + cursor] == pattern[rightBorder - suffixSize-1 + cursor]) {
-                cursor++
-                lps++;
+    var result = {};
+    result[0] = 0;
+    var i = 1;
+    var len = 0;
+    while (i < pattern.length) {
+        var pointer = "";
+        for (var j = 0; j <= i; j++) {
+            if (j < i) {
+                pointer += " ";
             } else {
-                break;
+                pointer += "|";
             }
         }
-        result.push(lps);
+        console.log("");
+        console.log(pointer);
+        var shift = i - len;
+        if (shift > 0) {
+            console.log(pattern);
+            var padding = "";
+            while (shift != 0) {
+                padding += "-";
+                shift--;
+            }
+            console.log(padding + pattern);
+        }
+        //console.log(pattern[i] + " and " + pattern[len]);
+        if (pattern[i] == pattern[len]) {
+            len++;
+            result[i] = len;
+            i++;
+        } else {
+            if (len != 0) {
+                len = result[len - 1];
+            } else {
+                result[i] = 0;
+                i++;
+            }
+        }
     }
     return result;
 }
 
 
-var result = calcLpsTable("ababab");
+var result = calcLpsTable("ababca");
 console.log(result);
