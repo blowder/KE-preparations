c4001f14e78f71bf1d209e4807e465b41a3f9be7
blowder <blowder@mail.ru>
2018-08-09 03:38:24 +0300

    Exchanger test

--- Concurrency/src/main/java/com/blowder/ThreadFlowControl.java
+++ Concurrency/src/main/java/com/blowder/ThreadFlowControl.java
@@ -3,6 +3,7 @@ package com.blowder;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Exchanger;
 import java.util.concurrent.Semaphore;
 
 public class ThreadFlowControl {
@@ -12,7 +13,7 @@ public class ThreadFlowControl {
         public void startForCountdown() {
             for (int i = 5; i > 0; i--) {
                 latch.countDown();
-                TestUtils.log("Counter is "+ i);
+                TestUtils.log("Counter is " + i);
                 TestUtils.sleep(50);
             }
         }
@@ -68,6 +69,42 @@ public class ThreadFlowControl {
         TestUtils.sleep(1000);
         TestUtils.log("-----------------------");
         semaphoreTest();
+        TestUtils.sleep(1000);
+        TestUtils.log("-----------------------");
+        exchangerTest();
+    }
+
+    public static class ExchangerUser {
+        private Exchanger<String> exchanger = new Exchanger<String>();
+
+        public void produce() {
+            try {
+                TestUtils.log("Ready to produce \"Hello\"");
+                TestUtils.sleep(1000);
+                this.exchanger.exchange("Hello from " + Thread.currentThread().getName() + "!!");
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+        public void consume() {
+            try {
+                TestUtils.log("Ready to consume \"Hello\"");
+                String result = this.exchanger.exchange(null);
+                TestUtils.log("Received message: \"" + result + "\"");
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void exchangerTest() {
+        ExchangerUser exchanger = new ExchangerUser();
+        new Thread(() -> exchanger.consume()).start();
+        new Thread(() -> exchanger.produce()).start();
+
     }
 
     private static void semaphoreTest() {
