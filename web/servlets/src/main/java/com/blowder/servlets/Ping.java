504abe83d6e41b39664832cbd8b1e6c4a890b495
blowder <blowder@mail.ru>
2018-10-03 03:25:04 +0300

    simple servlet

---
+++ web/servlets/src/main/java/com/blowder/servlets/Ping.java
@@ -0,0 +1,27 @@
+package com.blowder.servlets;
+
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/ping")
+public class Ping extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        String result = "<html><body>";
+        
+        Enumeration<String> names = req.getParameterNames();
+        while (names.hasMoreElements()) {
+            String element = (String) names.nextElement();
+            result += "<p>" + element + "=" + req.getParameter(element) + "</p>";
+        }
+        result += "</body></html>";
+        try (PrintWriter writer = resp.getWriter()) {
+            writer.append(result);
+        } catch (Exception e) {
+        }
+        resp.setStatus(201);
+
+    }
+}
